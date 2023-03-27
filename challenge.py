2fc821d9cf7549b64a4834026419b46eea0cf359
Nuno M. Félix <nunomfelix@MBP-de-Nuno.Home>
2023-03-27 23:04:37 +0100

    challenge and unit tests

---
+++ challenge.py
@@ -0,0 +1,123 @@
+import json
+
+def main():
+    data = json.load(open('./data.json'))
+    store = AptoideStore(data['store_id'], data['store_balance'], data['store_comission'],\
+    data['apps'],data['items'],data['users'])
+
+    while True:
+        _input = input()
+        _input_split = _input.split(" ")
+        
+        if(len(_input_split) != 3):
+            print("Invalid Input: Expected three strings separated by spaces")
+        else:
+            app, item, user = _input_split
+            store.purchase_transaction(app, item, user)
+
+class AptoideStore():
+    def __init__(self, id: str, balance: float, comission:float =0.25, apps:list=[], items:list=[], users:list=[]) -> None:
+        self.id = id
+        self.balance = balance
+        self.comission = comission
+        self.apps = {app['id']: App(app['id'],app['dev_id'],[item for item in items if item['app_id'] == app['id']], app['comission']) for app in apps}
+        self.users = {user['id']: User(user['id'], user['balance']) for user in users}
+        self.transactions = []
+
+    def purchase_transaction(self, app_id: str, item: str, sender: str) -> 'Transaction':
+        #TrivialDrive Oil User#123
+        try:
+            app = self.apps[app_id]
+            item = app.items[item]
+            sender = self.users[sender]
+            amount = item['price']
+            
+            if sender.balance < amount:
+                print(f"ERROR: User {sender.user_id} doesn't have enough balance to make this purchase")
+                return
+            
+            if self.comission + app.comission != 1:
+                print(f"ERROR: The comissions between the Store and the App don't add up to 100%")
+            
+            store_share=round(self.comission * amount, 2)
+            dev_share=round(app.comission * amount, 2)
+
+            sender.balance-=amount
+            self.balance+=store_share
+            self.users[app.dev_id].balance+=dev_share
+            
+            receivers={
+                self.id: store_share,
+                self.users[app.dev_id].user_id: dev_share
+            }
+            
+            balances={
+                sender.user_id: sender.balance,
+                self.users[app.dev_id].user_id: self.users[app.dev_id].balance,
+                self.id: self.balance
+            }
+            
+            tx_data=['PURCHASE', len(self.transactions)+1, app_id, item['id'], item['currency'], item['price'], sender.user_id, receivers]
+            transaction = Transaction(tx_data)
+            self.transactions.append(transaction)
+            self.users[sender.user_id].purchases.append({"app_id": app_id, "item_id": item['id'], "amount": amount})
+            print(transaction, '\n', f"BALANCE => {'; '.join([f'{user}: €{amount}' for user, amount in balances.items()])}")
+            
+            self.reward_transaction(sender.user_id, app_id, amount)
+            return transaction
+        
+        except Exception as err: 
+            raise err
+    
+    def reward_transaction(self, user_id: str, app_id: str, amount: float) -> 'Transaction':
+        count = len([purchase for purchase in self.users[user_id].purchases if purchase['app_id'] == app_id])
+        reward_percent = 0.05 if 2 <= count <= 10 else 0.10 if count >= 11 else None
+        if(reward_percent is None):
+            return
+        reward_amount = round(amount * reward_percent, 2)
+        self.users[user_id].balance+=reward_amount
+        self.balance-=reward_amount
+        tx_data=['REWARD', len(self.transactions)+1,'', '', '€', reward_amount, self.id, {user_id: reward_amount}]
+        tx = Transaction(tx_data)
+        self.transactions.append(tx)
+        
+        balances={
+            user_id: self.users[user_id].balance,
+            self.id: self.balance
+        }
+        print(count)
+        print("#########\n",tx, '\n', f"BALANCE => {'; '.join([f'{user}: €{amount:.2f}' for user, amount in balances.items()])}")
+        return tx
+    
+class User():
+    def __init__(self, user_id: list, balance: float) -> None:
+        self.user_id = user_id
+        self.balance = balance
+        self.purchases = []
+        
+    def __str__(self):
+        return f"User(id={self.user_id}, balance={self.balance})"
+    
+class App():
+    def __init__(self, id: str, dev_id: str, items: list, comission:float=0.75) -> None:
+        self.id = id
+        self.dev_id = dev_id
+        self.items = {item['id']: item for item in items}
+        self.comission = comission
+        
+    def __str__(self) -> str:
+        return f"App(id={self.id}, dev_id={self.dev_id}, items={self.items}, comission={self.comission})"
+
+class Transaction():
+    def __init__(self, tx_data: list) -> 'Transaction':
+        self.type, self.id, self.app_id, self.item_id, self.currency, self.amount, self.sender, self.receivers = tx_data
+        
+    def __str__(self) -> str:
+        if self.type == 'REWARD':
+            return f"{self.type} TRANSACTION => id: {self.id}; amount: {self.currency}{self.amount:.2f}; sender: {self.sender}; receivers: {'; '.join([f'{receiver}: {self.currency}{value}' for receiver, value in self.receivers.items()])}"
+        else:
+            return f"{self.type} TRANSACTION => id: {self.id}; app: {self.app_id}; item: {self.item_id}; amount: {self.currency}{self.amount:.2f}; sender: {self.sender}; receivers: {'; '.join([f'{receiver}: {self.currency}{value}' for receiver, value in self.receivers.items()])}"
+
+if __name__ == "__main__":
+    main()
+    
---
+++ tests.py
@@ -0,0 +1,74 @@
+import pytest
+import json
+from challenge import AptoideStore
+
+@pytest.fixture
+def test_data():
+    with open('./test_data.json') as f:
+        data = json.load(f)
+    return data
+
+def test_purchase_transaction(test_data):
+    # Initialize AptoideStore object
+    store = AptoideStore(test_data['store_id'], test_data['store_balance'], test_data['store_comission'],
+                         test_data['apps'], test_data['items'], test_data['users'])
+
+    # Test successful purchase transaction
+    tx1 = store.purchase_transaction('app1', 'item1', 'user1')
+    assert tx1.type == 'PURCHASE'
+    assert tx1.app_id == 'app1'
+    assert tx1.item_id == 'item1'
+    assert tx1.currency == '€'
+    assert tx1.amount == 1.0
+    assert tx1.sender == 'user1'
+    assert tx1.receivers == {'store1': 0.2, 'dev1': 0.8}
+    assert store.balance == 1.20
+    assert store.users['dev1'].balance == 0.80
+    assert store.users['user1'].balance == 9.0
+
+    # Test failed purchase transaction due to insufficient balance
+    tx2 = store.purchase_transaction('app1', 'item2', 'user2')
+    assert tx2 is None
+    assert store.users['user2'].balance == 0.5
+
+    # Test failed purchase transaction due to invalid app_id
+    with pytest.raises(KeyError):
+        store.purchase_transaction('invalid_app_id', 'item1', 'user1')
+
+    # Test failed purchase transaction due to invalid item_id
+    with pytest.raises(KeyError):
+        store.purchase_transaction('app1', 'invalid_item_id', 'user1')
+
+    # Test failed purchase transaction due to invalid user_id
+    with pytest.raises(KeyError):
+        store.purchase_transaction('app1', 'item1', 'invalid_user_id')
+
+def test_reward_transaction(test_data):
+    # Initialize AptoideStore object
+    store = AptoideStore(test_data['store_id'], test_data['store_balance'], test_data['store_comission'],
+                         test_data['apps'], test_data['items'], test_data['users'])
+    
+    [store.users['user1'].purchases.append({"app_id": "app1", "item_id": "item1", "amount": 1.00}) for _ in range(2)]
+
+    # Test reward transaction with count = 2
+    tx1 = store.reward_transaction('user1', 'app1', 1.0)
+
+    assert tx1.type == 'REWARD'
+    assert tx1.amount == 0.05
+    assert store.balance == 0.95
+    assert store.users['user1'].balance == 10.05
+    
+    [store.users['user1'].purchases.append({"app_id": "app1", "item_id": "item1", "amount": 1.00}) for _ in range(9)]
+    # Test reward transaction with count = 11
+    tx2 = store.reward_transaction('user1', 'app1', 1.0)
+    assert tx2.type == 'REWARD'
+    assert tx2.amount == 0.10
+    assert store.balance == 0.85
+    assert store.users['user1'].balance == 10.15
+
+    store.users['user1'].purchases = [{"app_id": "app1", "item_id": "item1", "amount": 1.00}]
+    # Test reward transaction with count = 1 (should not create a transaction)
+    tx3 = store.reward_transaction('user1', 'app1', 1.0)
+    assert tx3 is None
+    assert store.balance == 0.85
+    assert store.users['user1'].balance == 10.15
