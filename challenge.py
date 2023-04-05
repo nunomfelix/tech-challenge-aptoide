7b0e2b901535613ffbb8b8cb7ea507d81199ff93
Nuno Félix <nunomfelix@ua.pt>
2023-04-05 16:49:48 +0100

    updated challenge code with pydantic for object instantiation, refactoring classes, better error handling and input validation with more detailed error messages

--- challenge.py
+++ challenge.py
@@ -1,129 +1,179 @@
 import json
-import sys 
+import sys
+from typing import List, Dict, Optional
+from pydantic import BaseModel, ValidationError, validator
 
+class TransactionInput(BaseModel):
+    app_id: str 
+    item_id: str
+    user_id: str
+    
+class Transaction(BaseModel):
+    tx_type: str
+    id: int
+    app_id: str
+    item_id: str
+    currency: str
+    amount: float
+    sender: str
+    receivers: dict
+
+    def __str__(self) -> str:
+        if self.tx_type == 'REWARD':
+            return f"{self.tx_type} TRANSACTION => id: {self.id}; amount: {self.currency}{self.amount:.2f}; sender: {self.sender}; receivers: {'; '.join([f'{receiver}: {self.currency}{value}' for receiver, value in self.receivers.items()])}"
+        else:
+            return f"{self.tx_type} TRANSACTION => id: {self.id}; app: {self.app_id}; item: {self.item_id}; amount: {self.currency}{self.amount:.2f}; sender: {self.sender}; receivers: {'; '.join([f'{receiver}: {self.currency}{value}' for receiver, value in self.receivers.items()])}"
+
+class User(BaseModel):
+    id: str
+    balance: float
+    purchases: List[dict] = []
+
+    def __str__(self) -> str:
+        return f"User(id={self.id}, balance={self.balance})"
+
+class App(BaseModel):
+    id: str
+    dev_id: str
+    items: Dict[str, dict]
+    comission: float = 0.75
+    
+    @validator('items', pre=True)
+    def instantiate_items(cls, items):
+        return {item['id']: item for item in items}
+
+    def __str__(self) -> str:
+        return f"App(id={self.id}, dev_id={self.dev_id}, items={self.items}, comission={self.comission})"
+
+class AptoideStore(BaseModel):
+    store_id: str
+    store_balance: float
+    comission: float = 0.25
+    apps: Dict[str, App]
+    users: Dict[str, User]
+    transactions: Optional[List[Transaction]] = []
+    
+    @validator('apps', pre=True)
+    def instantiate_apps(cls, apps):
+        return {app['id']: App(**app) for app in apps}
+
+    @validator('users', pre=True)
+    def instantiate_users(cls, users):
+        return {user['id']: User(**user) for user in users}
+
+    def purchase_transaction(self, app_id: str, item_id: str, sender_id: str) -> 'Transaction':
+        try:
+            if app_id not in self.apps:
+                print(f"ERROR: Invalid AppId", file=sys.stderr)
+                return None
+
+            if sender_id not in self.users:
+                print(f"ERROR: Invalid SenderId", file=sys.stderr)
+                return None
+            
+            app = self.apps[app_id]
+            item = app.items[item_id]
+
+            if item is None:
+                print(f"ERROR: Invalid Item", file=sys.stderr)
+                return None
+
+            sender = self.users[sender_id]
+            amount = item['price']
+
+            if sender.balance < amount:
+                print(f"ERROR: User {sender.id} doesn't have enough balance to make this purchase")
+                return None
+
+            if self.comission + app.comission != 1:
+                print(f"ERROR: The comissions between the Store and the App don't add up to 100%")
+                return None
+
+            store_share = round(self.comission * amount, 2)
+            dev_share = round(app.comission * amount, 2)
+
+            sender.balance -= amount
+            self.store_balance += store_share
+            self.users[app.dev_id].balance += dev_share
+
+            receivers = {
+                self.users[app.dev_id].id: dev_share,
+                self.store_id: store_share
+            }
+
+            balances = {
+                sender.id: sender.balance,
+                self.users[app.dev_id].id: self.users[app.dev_id].balance,
+                self.store_id: self.store_balance
+            }
+
+            transaction = Transaction(
+                tx_type='PURCHASE',
+                id=len(self.transactions)+1,
+                app_id=app_id,
+                item_id=item_id,
+                currency=item['currency'],
+                amount=amount,
+                sender=sender.id,
+                receivers=receivers
+            )
+            self.transactions.append(transaction)
+            sender.purchases.append({"app_id": app_id, "item_id": item_id, "amount": amount})
+            print(transaction, '\n', f"BALANCE => {'; '.join([f'{user}: €{amount:.2f}' for user, amount in balances.items()])}")
+
+            self.reward_transaction(sender.id, app_id, amount)
+            return transaction
+
+        except Exception as err:
+            raise err
+
+    def reward_transaction(self, user_id: str, app_id: str, amount: float) -> 'Transaction':
+        count = len([purchase for purchase in self.users[user_id].purchases if purchase['app_id'] == app_id])
+        reward_percent = 0.05 if 2 <= count <= 10 else 0.10 if count >= 11 else None
+        if reward_percent is None:
+            return
+
+        reward_amount = round(amount * reward_percent, 2)
+        self.users[user_id].balance += reward_amount
+        self.store_balance -= reward_amount
+
+        transaction = Transaction(
+            tx_type='REWARD',
+            id=len(self.transactions) + 1,
+            app_id='',
+            item_id='',
+            currency='€',
+            amount=reward_amount,
+            sender=self.store_id,
+            receivers={user_id: reward_amount}
+        )
+        self.transactions.append(transaction)
+
+        balances = {
+            user_id: self.users[user_id].balance,
+            self.store_id: self.store_balance
+        }
+        
+        print("#########\n", transaction, '\n', f"BALANCE => {'; '.join([f'{user}: €{amount:.2f}' for user, amount in balances.items()])}")
+        return transaction 
+    
 def main():
     data = json.load(open('./data.json'))
-    store = AptoideStore(data['store_id'], data['store_balance'], data['store_comission'],\
-    data['apps'],data['items'],data['users'])
+    store = AptoideStore(**data)
 
     while True:
         _input = input()
         _input_split = _input.split(" ")
-        
-        if(len(_input_split) != 3):
-            print("Invalid Input: Expected three inputs separated by spaces")
-        else:
-            app, item, user = _input_split
-            store.purchase_transaction(app, item, user)
 
-class AptoideStore():
-    def __init__(self, id: str, balance: float, comission:float =0.25, apps:list=[], items:list=[], users:list=[]) -> None:
-        self.id = id
-        self.balance = balance
-        self.comission = comission
-        self.apps = {app['id']: App(app['id'],app['dev_id'],[item for item in items if item['app_id'] == app['id']], app['comission']) for app in apps}
-        self.users = {user['id']: User(user['id'], user['balance']) for user in users}
-        self.transactions = []
-
-    def purchase_transaction(self, app_id: str, item: str, sender: str) -> 'Transaction':
         try:
-            if app_id not in self.apps or \
-                item not in self.apps[app_id].items or \
-                sender not in self.users:
-                print(f"ERROR: Invalid AppId/Item/Sender", file=sys.stderr)
-                return
-            
-            app = self.apps[app_id]
-            item = app.items[item]
-            sender = self.users[sender]
-            amount = item['price']
-            
-            if sender.balance < amount:
-                print(f"ERROR: User {sender.user_id} doesn't have enough balance to make this purchase")
-                return
-            
-            if self.comission + app.comission != 1:
-                print(f"ERROR: The comissions between the Store and the App don't add up to 100%")
-                return
-            
-            store_share=round(self.comission * amount, 2)
-            dev_share=round(app.comission * amount, 2)
-
-            sender.balance-=amount
-            self.balance+=store_share
-            self.users[app.dev_id].balance+=dev_share
-            
-            receivers={
-                self.users[app.dev_id].user_id: dev_share,
-                self.id: store_share
-            }
-            
-            balances={
-                sender.user_id: sender.balance,
-                self.users[app.dev_id].user_id: self.users[app.dev_id].balance,
-                self.id: self.balance
-            }
-            
-            tx_data=['PURCHASE', len(self.transactions)+1, app_id, item['id'], item['currency'], item['price'], sender.user_id, receivers]
-            transaction = Transaction(tx_data)
-            self.transactions.append(transaction)
-            self.users[sender.user_id].purchases.append({"app_id": app_id, "item_id": item['id'], "amount": amount})
-            print(transaction, '\n', f"BALANCE => {'; '.join([f'{user}: €{amount:.2f}' for user, amount in balances.items()])}")
-            
-            self.reward_transaction(sender.user_id, app_id, amount)
-            return transaction
+            transaction_input = TransactionInput(app_id=_input_split[0], item_id=_input_split[1], user_id=_input_split[2])
+            store.purchase_transaction(transaction_input.app_id, transaction_input.item_id, transaction_input.user_id)
         
-        except Exception as err: 
-            raise err
-    
-    def reward_transaction(self, user_id: str, app_id: str, amount: float) -> 'Transaction':
-        count = len([purchase for purchase in self.users[user_id].purchases if purchase['app_id'] == app_id])
-        reward_percent = 0.05 if 2 <= count <= 10 else 0.10 if count >= 11 else None
-        if(reward_percent is None):
-            return
-        reward_amount = round(amount * reward_percent, 2)
-        self.users[user_id].balance+=reward_amount
-        self.balance-=reward_amount
-        tx_data=['REWARD', len(self.transactions)+1,'', '', '€', reward_amount, self.id, {user_id: reward_amount}]
-        tx = Transaction(tx_data)
-        self.transactions.append(tx)
-        
-        balances={
-            user_id: self.users[user_id].balance,
-            self.id: self.balance
-        }
-        print("#########\n",tx, '\n', f"BALANCE => {'; '.join([f'{user}: €{amount:.2f}' for user, amount in balances.items()])}")
-        return tx
-    
-class User():
-    def __init__(self, user_id: list, balance: float) -> None:
-        self.user_id = user_id
-        self.balance = balance
-        self.purchases = []
-        
-    def __str__(self) -> str:
-        return f"User(id={self.user_id}, balance={self.balance})"
-    
-class App():
-    def __init__(self, id: str, dev_id: str, items: list, comission:float=0.75) -> None:
-        self.id = id
-        self.dev_id = dev_id
-        self.items = {item['id']: item for item in items}
-        self.comission = comission
-        
-    def __str__(self) -> str:
-        return f"App(id={self.id}, dev_id={self.dev_id}, items={self.items}, comission={self.comission})"
-
-class Transaction():
-    def __init__(self, tx_data: list) -> 'Transaction':
-        self.type, self.id, self.app_id, self.item_id, self.currency, self.amount, self.sender, self.receivers = tx_data
-        
-    def __str__(self) -> str:
-        if self.type == 'REWARD':
-            return f"{self.type} TRANSACTION => id: {self.id}; amount: {self.currency}{self.amount:.2f}; sender: {self.sender}; receivers: {'; '.join([f'{receiver}: {self.currency}{value}' for receiver, value in self.receivers.items()])}"
-        else:
-            return f"{self.type} TRANSACTION => id: {self.id}; app: {self.app_id}; item: {self.item_id}; amount: {self.currency}{self.amount:.2f}; sender: {self.sender}; receivers: {'; '.join([f'{receiver}: {self.currency}{value}' for receiver, value in self.receivers.items()])}"
+        except IndexError:
+            print("Invalid Input: Expected three inputs separated by spaces")
+            
+        except ValidationError as e:
+            print(f"Invalid Input: {e}")
 
 if __name__ == "__main__":
     main()
-    
--- tests.py
+++ tests.py
@@ -1,9 +1,7 @@
 import pytest
 import json
-import sys
-from io import StringIO
-
 from challenge import AptoideStore
+from pydantic import ValidationError
 
 @pytest.fixture
 def test_data():
@@ -11,36 +9,22 @@ def test_data():
         data = json.load(f)
     return data
 
-def test_purchase_transaction(test_data, monkeypatch):
+def test_purchase_transaction(test_data):
+    
     # Initialize AptoideStore object
-    store = AptoideStore(test_data['store_id'], test_data['store_balance'], test_data['store_comission'],
-                         test_data['apps'], test_data['items'], test_data['users'])
+    store = AptoideStore(**test_data)
 
-    error_output = StringIO()
-    monkeypatch.setattr(sys, 'stderr', error_output)
-    # Test failed purchase transaction due to invalid app_id
-    store.purchase_transaction('invalid_app_id', 'item1', 'user1')
-    assert 'ERROR: Invalid AppId/Item/Sender' in error_output.getvalue()
-    
-    # Test failed purchase transaction due to invalid item_id
-    store.purchase_transaction('app1', 'invalid_item_id', 'user1')
-    assert 'ERROR: Invalid AppId/Item/Sender' in error_output.getvalue()
-    
-    # Test failed purchase transaction due to invalid user_id
-    store.purchase_transaction('app1', 'item1', 'invalid_user_id')
-    assert 'ERROR: Invalid AppId/Item/Sender' in error_output.getvalue()
-    
     # Test successful purchase transaction
     tx1 = store.purchase_transaction('app1', 'item1', 'user1')
-    assert tx1.type == 'PURCHASE'
+    print(tx1)
     assert tx1.app_id == 'app1'
     assert tx1.item_id == 'item1'
     assert tx1.currency == '€'
     assert tx1.amount == 1.0
     assert tx1.sender == 'user1'
-    assert tx1.receivers == {'store1': 0.2, 'dev1': 0.8}
-    assert store.balance == 1.20
-    assert store.users['dev1'].balance == 0.80
+    assert tx1.receivers == {'store1': 0.25, 'dev1': 0.75}
+    assert store.store_balance == 1.25
+    assert store.users['dev1'].balance == 0.75
     assert store.users['user1'].balance == 9.0
 
     # Test failed purchase transaction due to insufficient balance
@@ -50,29 +34,30 @@ def test_purchase_transaction(test_data, monkeypatch):
 
 def test_reward_transaction(test_data):
     # Initialize AptoideStore object
-    store = AptoideStore(test_data['store_id'], test_data['store_balance'], test_data['store_comission'],
-                         test_data['apps'], test_data['items'], test_data['users'])
+    store = AptoideStore(**test_data)
     
     [store.users['user1'].purchases.append({"app_id": "app1", "item_id": "item1", "amount": 1.00}) for _ in range(2)]
+
     # Test reward transaction with count = 2
     tx1 = store.reward_transaction('user1', 'app1', 1.0)
 
-    assert tx1.type == 'REWARD'
+    assert tx1.tx_type == 'REWARD'
     assert tx1.amount == 0.05
-    assert store.balance == 0.95
+    assert store.store_balance == 0.95
     assert store.users['user1'].balance == 10.05
     
     [store.users['user1'].purchases.append({"app_id": "app1", "item_id": "item1", "amount": 1.00}) for _ in range(9)]
     # Test reward transaction with count = 11
     tx2 = store.reward_transaction('user1', 'app1', 1.0)
-    assert tx2.type == 'REWARD'
+    assert tx2.tx_type == 'REWARD'
     assert tx2.amount == 0.10
-    assert store.balance == 0.85
+    assert store.store_balance == 0.85
     assert store.users['user1'].balance == 10.15
 
     store.users['user1'].purchases = [{"app_id": "app1", "item_id": "item1", "amount": 1.00}]
     # Test reward transaction with count = 1 (should not create a transaction)
     tx3 = store.reward_transaction('user1', 'app1', 1.0)
     assert tx3 is None
-    assert store.balance == 0.85
+    assert store.store_balance == 0.85
     assert store.users['user1'].balance == 10.15
+    
